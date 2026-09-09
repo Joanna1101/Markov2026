67b4e4ca36e919ca9c3fcd1c5937d6956c13d312
Joanna1101 <joanna.parker@colorado.edu>
2026-09-09 15:56:14 -0600

    Problem 3 done!

--- Homework 2/HW2_Prob3.py
+++ Homework 2/HW2_Prob3.py
@@ -1,5 +1,6 @@
 ## IMPORTS ##########################################
 #####################################################
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 from prettytable import PrettyTable
@@ -7,18 +8,26 @@ from prettytable import PrettyTable
 
 ## Plotting #########################################
 #####################################################
-def plothist(accepted, name):
+def plothist(accepted, lam, name):
     """
     Plots normalized histogram with f(x) overlaid
     Inputs:  accepted....(list) accepted samples
+             lam.........(float) lambda for naming
              name........(string) desired plot name
     Outputs: name.png
     """
-    f = lambda x: x*np.exp(-x)
-    # TODO
-    plt.plot(accepted)
+    # Making f
+    xs = np.arange(0, max(accepted), 0.01)
+    f = lambda x: x*np.exp(-x) 
+    
+    plt.plot(xs, f(xs), color = "indigo", label = "f(x)")
+    plt.hist(accepted, bins=50, density = True, color='thistle', edgecolor = 'purple', label = "Accepted Samples")
+    plt.legend()
+    plt.xlabel('Accepted Samples')
+    plt.ylabel('Frequency')
+    plt.title(f'Normalized Histogram vs f(x), \u03BB = {lam}')
     plt.savefig(f"{name}.png")
-    # Pinks I Thinks tee hee
+    plt.close()
     
 
 ## Acceptance-Rejection Method ######################
@@ -34,21 +43,22 @@ def accept_reject(N, lam, c):
              total.......(int) total number of samples
     """
     total = 0
-    prev_accepted = 0
     accepted = []
-    times = []
+    start = time.perf_counter()
+    
     while(len(accepted) < N):
-        U1 = np.random.uniform()
-        U2 = np.random.uniform()
-        X = -(1/lam)*np.ln(U1)
+        U1 = np.random.uniform(0,1)
+        U2 = np.random.uniform(0,1)
+        X = -(1/lam)*np.log(U1)
         
-        if (U2 <= (1/lam*c)*X*np.exp((lam-1)*X)):
+        if (U2 <= (1/(lam*c))*X*np.exp((lam-1)*X)):
             accepted.append(X)
-            times.append(total-prev_accepted)
-            prev_accepted = total
         total +=1 
         
-    return accepted, times, total
+    end = time.perf_counter()
+    elapsed = end-start
+    
+    return accepted, elapsed, total
     
 
 ## MAIN #############################################
@@ -57,26 +67,26 @@ if __name__ == "__main__":
     N = 10e4
     lambda1 = 0.5
     lambda2 = 0.2
-    c1 = 4/np.e
-    c2 = 1/(0.16*np.e)
+    c1 = 4/np.exp(1)
+    c2 = 1/(0.16*np.exp(1))
     
-    accepted1, times1, total1 = accept_reject(N, lambda1, c1)
-    accepted2, times2, total2 = accept_reject(N, lambda2, c2)
+    accepted1, elapsed1, total1 = accept_reject(N, lambda1, c1)
+    accepted2, elapsed2, total2 = accept_reject(N, lambda2, c2)
     
-    plothist(accepted1, r"$\lambda$ = 0.5")
-    plothist(accepted2, r"$\lambda$ = 0.2")
+    plothist(accepted1, 0.5, "lambda=0.5")
+    plothist(accepted2, 0.2, "lambda=0.2")
     
-    # empirical acceptance fraction
-    fraction1 = len(accepted1)/len(total1)
-    fraction2 = len(accepted2)/len(total2)
+    # Empirical acceptance fraction
+    fraction1 = len(accepted1)/total1
+    fraction2 = len(accepted2)/total2
     
-    # mean time per accepted sample
-    mean1 = np.average(times1)
-    mean2 = np.average(times2)
-    
-    # TODO: round numbers so dey look nice
+    # Mean Time
+    mean1 = elapsed1/N
+    mean2 = elapsed2/N
+
+    # A Pretty Table :)
     table = PrettyTable()
-    table.fieldnames = [r"$\lambda$", "Empirical Fraction", "1/c", "Mean Time"]
-    table.addrow([lambda1, fraction1, 1/c1, mean1])
-    table.addrow([lambda2, fraction2, 1/c2, mean2])
+    table.field_names = ["\u03BB", "Empirical Fraction", "1/c", "Mean Time"]
+    table.add_row([lambda1, round(fraction1,4), round(1/c1,4), f"{mean1:.4e}"])
+    table.add_row([lambda2, round(fraction2,4), round(1/c2,4), f"{mean2:.4e}"])
     print(table)
