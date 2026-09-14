3e6d647629cb0157f36c05f04aba3c11884880fd
Joanna1101 <joanna.parker@colorado.edu>
2026-09-14 10:00:37 -0600

    files for hw 3

---
+++ Homework 3/HW3_Prob1.py
@@ -0,0 +1,11 @@
+## IMPORTS ##########################################
+#####################################################
+import numpy as np
+import matplotlib.pyplot as plt
+from prettytable import PrettyTable
+
+
+## MAIN #############################################
+#####################################################
+if __name__ == "__main__":
+    print("hello world!")
---
+++ Homework 3/HW3_Prob2.py
@@ -0,0 +1,10 @@
+## IMPORTS ##########################################
+#####################################################
+import numpy as np
+import matplotlib.pyplot as plt
+from prettytable import PrettyTable
+
+## MAIN #############################################
+#####################################################
+if __name__ == "__main__":
+    print("hello world!")
