8eb3258eff86bc415b72f8b29c286e631e057ae9
Joanna1101 <joanna.parker@colorado.edu>
2026-09-16 22:03:48 -0600

    Code for 1, 2 done yay!

--- Homework 3/HW3_Prob1.py
+++ Homework 3/HW3_Prob1.py
@@ -1,11 +1,19 @@
 ## IMPORTS ##########################################
 #####################################################
 import numpy as np
-import matplotlib.pyplot as plt
-from prettytable import PrettyTable
-
 
 ## MAIN #############################################
 #####################################################
 if __name__ == "__main__":
-    print("hello world!")
+    p = [[(9/10), (1/10), 0],[0, (3/4), (1/4)],[(1/2), 0, (1/2)]]
+    p50 = np.linalg.matrix_power(p, 50)
+    print("\np^50 via numpy matrix_power:")
+    print(p50)
+    
+    f = p50[0]
+    print("\nLong run fraction (fW, fC, fS):")
+    print(f)
+    
+    dwell = [i*16 for i in f]
+    print("\nDwell times = f*16 ms")
+    print(f"{dwell}\n")
--- Homework 3/HW3_Prob2.py
+++ Homework 3/HW3_Prob2.py
@@ -1,10 +1,27 @@
 ## IMPORTS ##########################################
 #####################################################
 import numpy as np
-import matplotlib.pyplot as plt
-from prettytable import PrettyTable
 
 ## MAIN #############################################
 #####################################################
 if __name__ == "__main__":
-    print("hello world!")
+    p = np.array([[(1/2), (1/2), 0, 0, 0, 0],
+         [(1/3), 0, (1/3), 0, (1/3), 0],
+         [0, 0, (1/4), (3/4), 0, 0],
+         [0, 0, 1, 0, 0, 0],
+         [0, 0, 0, 0, 0, 1],
+         [0, 0, 0, 0, 1, 0]])
+    
+    p20 = np.linalg.matrix_power(p, 20)
+    p21 = np.linalg.matrix_power(p, 21)
+    
+    # np.set_printoptions(formatter={'float_kind': '{: .4e}'.format})
+    np.set_printoptions(suppress = True)
+    print("\np20:")
+    print(p20)
+    print("\np21")
+    print(p21)
+    print(f"\n\n(p^20) at (1,5):{p20[0, 4]:.4f}")
+    print(f"(p^20) at (1,6):{p20[0, 5]:.4f}")
+    print(f"Sum: {p20[0, 4]+p20[0, 5]:.4f}")
+    
